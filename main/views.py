2ba1333ebf445291a363e49442978d555aa75530
rr-26 <raghavramani.31@gmail.com>
2023-06-07 01:51:19 +0530

    small changes

--- main/views.py
+++ main/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from main import models
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html', {})
+
+    latest_articles = models.Article.objects.all().order_by('-createdAt')[:10]
+    context = {
+        "latest_articles": latest_articles
+    }
+
+    return render(request, 'main/index.html', context)
